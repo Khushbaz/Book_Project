a9c1814fa0baa47c7e07c0d341c02649a1bd2c7c
Khushbaz <71674012+Khushbaz@users.noreply.github.com>
2021-04-19 13:59:12 +1200

    Chnages in the file

--- Book_Project/Models/Book.cs
+++ Book_Project/Models/Book.cs
@@ -9,10 +9,15 @@ namespace Book_Project.Models
     public class Book
     {
         [Key]
+        //This is the primary key
         public int Book_ID { get; set; }
+         //This is the variable to store the book title
         public string Book_Title { get; set; }
+         //This is the variable to store the author
         public string Author { get; set; }
+         //This is the variable to store the publisher
         public string Publisher { get; set; }
+         //This is the variable to store the year of publish
         public int Year_of_publish { get; set; }
     }
 }
